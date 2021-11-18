d9584ea5b974fae554d5df06d306ccc9e618be0c
GitPopcorn <370921291@qq.com>
2021-11-18 17:08:08 +0800

    Update vscode engines and other dependencies, fix the problems of lifecycle vscode:prepublish and compile.
    - Update vscode engines to 1.62.0.
    - Update all the dev dependencies to the latest version as yo code template.
    - Change the test parser to @vscode/test-electron and update index.js, runTest.js according to yo code template.

--- test/suite/index.js
+++ test/suite/index.js
@@ -5,10 +5,9 @@ const glob = require('glob');
 function run() {
 	// Create the mocha test
 	const mocha = new Mocha({
-		ui: 'tdd'
+		ui: 'tdd',
+		color: true
 	});
-	// Use any mocha API
-	mocha.useColors(true);
 
 	const testsRoot = path.resolve(__dirname, '..');
 
@@ -31,6 +30,7 @@ function run() {
 					}
 				});
 			} catch (err) {
+				console.error(err);
 				e(err);
 			}
 		});
